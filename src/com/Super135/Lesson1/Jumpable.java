21a5e7c1dd0ce1563d433acadb2367fa51abc888
Super-135 <super05061982@gmail.com>
2020-04-16 17:17:21 +0300

    Lesson1

--- src/com/Super135/Lesson1/Jumpable.java
+++
@@ -1,5 +0,0 @@
-package com.Super135.Lesson1;
-
-public interface Jumpable {
-    void jumpAction (double height);
-}
--- src/com/Super135/Lesson1/Runnable.java
+++
@@ -1,5 +0,0 @@
-package com.Super135.Lesson1;
-
-public interface Runnable {
-    void runAction(int length);
-}
